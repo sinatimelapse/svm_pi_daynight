28d4134063970e929ad8f0b2d446ffbce85a0d1d
sinatimelapse <99742413+sinatimelapse@users.noreply.github.com>
2022-02-15 15:34:45 +0100

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,60 @@
+import time
+import sys
+import os
+import cv2
+import numpy as np
+from sklearn.svm import SVC
+#from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
+import pickle as cPickle
+
+SHAPE = (20, 20)
+
+def read_files(directory):
+   print("Reading files...")
+   images_list = list()
+   files = os.listdir(directory)
+   for filename in files:
+      images_list.append(filename)
+   return images_list 
+   
+
+def extract_feature(image_file):
+   img = cv2.imread(image_file)
+   img = cv2.resize(img, SHAPE, interpolation = cv2.INTER_CUBIC)
+   img = img.flatten()
+   #img = img/np.mean(img)
+   return img
+
+if __name__ == "__main__":
+   if len(sys.argv) < 2:
+      print "Usage: python extract_features.py [image_folder]"
+      exit()
+
+   # Directory containing subfolders with images in them.
+   directory = sys.argv[1]
+
+   # generating two numpy arrays for features and labels
+   images= read_files(directory)
+   
+
+	# load saved model
+with open('svm_model.pkl' , 'rb') as f :
+    model = cPickle.load(f)
+    
+
+for i in images :
+   
+   path = './testes/%s'%i
+   img = extract_feature(path) 
+   
+   img = img.reshape(1, -1)
+   print "make a prediction"
+
+   label=model.predict(img)
+
+   print "i,'',label" 
+   if (label =="day") :
+      print "yes it is the daylight !"
+   
+
